317c7c2abdd8f75fb1fafa8b6ad3df57d12b7229
josephbill <josephbill00@gmail.com>
2024-07-04 10:09:38 +0300

    object ball deliverables

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -26,7 +26,7 @@ function gameObject(){
               },
               "Brook Lopez": {
                 number: 11,
-                shoe: 17,
+                shoe: 21,
                 points: 17,
                 rebounds: 19,
                 assists: 10,
@@ -152,3 +152,141 @@ function gameObject(){
       }
 
       console.log("Shoe Size" ,shoeSize("Brendan Haywood"))
+
+
+    function teamColors(teamname){
+      let object = gameObject()
+      if(object.home.teamName === teamname){
+         return object.home.colors
+      } else if(object.away.teamName === teamname){
+         return object.away.colors
+      } else {
+         return 'team not found in object'
+      }
+    }
+
+    // === : strick equality operator 
+    // == : loose equality operator 
+    console.log("Teams Jersey color ",teamColors("Charlotte Hornets"))
+
+    function teamNames(){
+      let object = gameObject()
+      let teams = [object.home.teamName,object.away.teamName]
+      return teams
+    }
+
+    console.log("team names " , teamNames())
+
+    // 1. variable to reference the object 
+    // 2. conditional to check if the team name passed belongs to the home or away key
+    // 3. access the players object 
+    // 4. iterate over all object elements : iterating over each player : 
+    // capture the jersey number for each player
+    // 5. package each players jersey number into an array 
+    // 6. return my array. 
+
+    function playerNumbers(teamname){
+       let object = gameObject()
+       let jerseyNumbers = []
+       let playersobject;
+       if(object.home.teamName === teamname){
+          playersobject = object.home.players
+          for(let player in playersobject){
+            console.log(player)
+            jerseyNumbers.push(object.home.players[player].number)
+          }
+
+       } else if(object.away.teamName === teamname) {
+          playersobject = object.away.players
+          for(let player in playersobject){
+            console.log(player)
+            jerseyNumbers.push(object.away.players[player].number)
+          }
+       } else {
+         return 'team not found'
+       }
+
+       return jerseyNumbers
+    }
+
+    console.log(playerNumbers("Brooklyn Nets"))
+
+    function bigshoeRebounds(){
+      let object = gameObject()
+      // variable that will be used to update the largest shoesize as we iterate over player objects in the object 
+      // variable to store info for the player with the largest shoe size 
+      // variable to store the player name 
+      // find the player association according to home or away key info 
+      // iterate over the players object 
+      // am checking on the shoesize on iteration and comparing it to the current largest shoe size 
+
+      // this keeps track of the largest size as we iterate over the players
+      let shoeSize = 0
+      // a variable to store the info about the player with the largest size 
+      let playerWithLargestShoeSize = {}
+      // a variable to store the player name 
+      let playername = '';
+
+      for(let player in object.home.players){
+        if(object.home.players[player].shoe > shoeSize ){
+          shoeSize = object.home.players[player].shoe
+          playerWithLargestShoeSize = object.home.players[player]
+          playername = player
+        }
+      }
+
+      for(let player in object.away.players){
+        if(object.away.players[player].shoe > shoeSize ){
+          shoeSize = object.away.players[player].shoe
+          playerWithLargestShoeSize = object.away.players[player]
+          playername = player
+        }
+      }
+
+      return {
+          [playername] : playerWithLargestShoeSize
+      };
+    }
+
+    console.log(bigshoeRebounds())
+
+    function bigCollectionshoeRebounds(){
+      let object = gameObject()
+      // variable that will be used to update the largest shoesize as we iterate over player objects in the object 
+      // variable to store info for the player with the largest shoe size 
+      // variable to store the player name 
+      // find the player association according to home or away key info 
+      // iterate over the players object 
+      // am checking on the shoesize on iteration and comparing it to the current largest shoe size 
+
+      // this keeps track of the largest size as we iterate over the players
+      let shoeSize = 0
+      // a variable to store the info about the player with the largest size 
+      let playerWithLargestShoeSize = []
+      // a variable to store the player name 
+      let playername = '';
+
+      for(let player in object.home.players){
+        if(object.home.players[player].shoe > shoeSize ){
+          shoeSize = object.home.players[player].shoe
+          playerWithLargestShoeSize = [{[player]: object.home.players[player]}]
+          playername = player
+        } else if(object.home.players[player].shoe === shoeSize){
+          playerWithLargestShoeSize.push({[player]: object.home.players[player]})
+        }
+      }
+
+      for(let player in object.away.players){
+        if(object.away.players[player].shoe > shoeSize ){
+          shoeSize = object.away.players[player].shoe
+          playerWithLargestShoeSize = [{[player]: object.away.players[player]}]
+          playername = player
+        } else if(object.away.players[player].shoe === shoeSize){
+          playerWithLargestShoeSize.push({[player]: object.away.players[player]})
+        }
+      }
+
+      return playerWithLargestShoeSize
+    }
+
+    console.log(bigCollectionshoeRebounds())
